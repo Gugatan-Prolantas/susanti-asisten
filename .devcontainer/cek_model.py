bc3ac0a7bda5ea1c2d9992e31130860e0ab9c4de
Gugatan-Prolantas <panmud.gugatan.papwt@gmail.com>
2026-05-12 21:29:31 +0700

    Delete .devcontainer/cek_model.py

--- .devcontainer/cek_model.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyD4aYZnmeArnKP9FHI5g6qTy6wIuvYxetY")
-for m in genai.list_models():
-    if 'embedContent' in m.supported_generation_methods:
-        print(m.name)
